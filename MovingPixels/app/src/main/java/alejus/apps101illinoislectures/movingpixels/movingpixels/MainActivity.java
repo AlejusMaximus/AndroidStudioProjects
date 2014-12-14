ae7158e4632a7b8c750bc9cff625c835edecf271
AlejusMaximus <aleix.pascual@gmail.com>
2014-12-14 18:28:01 +0100

    8 - 1 - 7.1. Preferences, views and interfaces

--- MovingPixels/app/src/main/java/alejus/apps101illinoislectures/movingpixels/movingpixels/MainActivity.java
+++ MovingPixels/app/src/main/java/alejus/apps101illinoislectures/movingpixels/movingpixels/MainActivity.java
@@ -8,24 +8,50 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Bitmap mBitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bitmap b = Bitmap.createBitmap(4,4, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(b);
+        mBitmap = Bitmap.createBitmap(4,4, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(mBitmap);
         Paint paint = new Paint();
         //Let's draw something on the bitmap then:
         c.drawColor(0xff808080); //gray color or grey
+        //Configure paint color (blue)
+        paint.setColor(0xff0000ff);
+        //lets draw a line:
+        c.drawLine(0,0,3,3,paint);
 
-        ImageView image = new ImageView(this);
-        image.setImageBitmap(b);
+        //Let's say goodbye to ImageView
+        //ImageView image = new ImageView(this);
 
-        setContentView(image);
+        //Now we will create our own view:
+        View v = new View(this){
+            @Override
+            protected void onDraw(Canvas canvas) {
+                //This new canvas corresponds to the new canvas of the all display
+                canvas.drawColor(0xffff9900);
+                float scaleX = this.getWidth() / ((float) mBitmap.getWidth());
+                float scaleY = this.getHeight() / ((float) mBitmap.getHeight());
+                // Log.d("MainActivity","Scale:"+scaleX+","+scaleY);
+                canvas.scale(scaleX, scaleY);
+
+                canvas.drawBitmap(mBitmap,0,0,null);
+
+
+            }
+        };
+
+        //image.setImageBitmap(b);
+
+        setContentView(v);
     }
 
 
