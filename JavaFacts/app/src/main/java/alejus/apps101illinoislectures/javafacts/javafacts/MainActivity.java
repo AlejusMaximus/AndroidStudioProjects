ac271899f41c1b193bdd51f83912a2a0fbb679fd
AlejusMaximus <aleix.pascual@gmail.com>
2015-01-01 23:54:10 +0100

    Lecture 10-1 Java facts app finished! :-)

--- JavaFacts/app/src/main/java/alejus/apps101illinoislectures/javafacts/javafacts/MainActivity.java
+++ JavaFacts/app/src/main/java/alejus/apps101illinoislectures/javafacts/javafacts/MainActivity.java
@@ -1,9 +1,11 @@
 package alejus.apps101illinoislectures.javafacts.javafacts;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1)  find our text view and set the text in it:
+        TextView message = (TextView) findViewById(R.id.message);
+        //2) we need our resources
+        Resources resources = this.getResources();
+        //3) We call this resources
+        String[] javafacts = resources.getStringArray(R.array.messages);
+        //So now we have our whole array (30 different strings) of java facts
+        //How to select one of them ? The second one for instance...
+        //String fact = javafacts[1];
+        //What if we want to do it random ? we will need to import math:
+        int index = (int) (Math.random()*javafacts.length);
+        String fact = javafacts[index];
+        //Now we pass fact
+        message.setText(fact);
+        //message.setText("HelloWorld!");
+
     }
 
 
