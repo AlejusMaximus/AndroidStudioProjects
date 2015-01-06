bf2eb99a2e958973ed30d39039235c195ee1e057
AlejusMaximus <aleix.pascual@gmail.com>
2015-01-06 13:18:25 +0100

    update sensor project

--- Sensores/app/src/main/java/alejus/upv/sensores/sensores/MainActivity.java
+++ Sensores/app/src/main/java/alejus/upv/sensores/sensores/MainActivity.java
@@ -1,17 +1,46 @@
 package alejus.upv.sensores.sensores;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Link with our layout*/
+        output = (TextView) findViewById(R.id.output);
+        /*The sensor manager is used to manage the sensor hardware available on Android devices
+        * Use <getSystemService> to return a reference to the Sensor Manager Service:*/
+        String service_name = Context.SENSOR_SERVICE;
+        SensorManager sensorManager = (SensorManager) getSystemService(service_name);
+        /*Rather than interacting with the sensor hardware directly, they are represented by
+        * sensor objects that describe the properties of the hardware sensor that represents.
+        * Now its time to find the sensors available in our device:*/
+        List<Sensor> allSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        /*All sensors available are saved in allSensor list*/
+        for (Sensor sensor: allSensors){
+            showSensor(sensor.getName());
+        }
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+
+    private void showSensor(String string) {
+        output.append(string + "\n");
     }
 
 
