af8d4178a76edcf4143100dc5f90dda348625c5b
AlejusMaximus <aleix.pascual@gmail.com>
2015-01-28 00:40:39 +0100

    Bluetooth 2.0 EDR

--- Bluev2EDR/app/src/main/java/alejusmaximus/apps101/bluev2edr/MainActivity.java
+++ Bluev2EDR/app/src/main/java/alejusmaximus/apps101/bluev2edr/MainActivity.java
@@ -22,25 +22,69 @@ THE SOFTWARE.
 */
 
 /*
-The way to treat BLE is not the same as bluetooth v2.0 + EDR (SPP profile)
-http://developer.android.com/guide/topics/connectivity/bluetooth-le.html
+This code has been designed to work with:
+
+1) JY-MCU Bluetooth to UART Wireless Serial Port -> TTL Converter needed (two resistors)
+    Baudrate = 38400 bps ; key = 1234;
+
+2) Modem Bluetooth BlueSMIRF Gold RN-41
+
+http://tienda.bricogeek.com/modulos-radiofrecuencia/242-modem-bluetooth-bluesmirf-gold.html
+
+General features for both:  Bluetooth V2.0+EDR (Enhanced Data Rate) 3Mbps
+                            Bluetooth SPP (Serial Port Protocol)
+
  */
 
-package alejusmaximus.apps101.ble;
+package alejusmaximus.apps101.bluev2edr;
 
-
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends ActionBarActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        STEP 1: GET BLUETOOTH ADAPTER
+        The BluetoothAdapter is required for any and all Bluetooth activity. To get the BluetoothAdapter,
+        call the static getDefaultAdapter() method. This returns a BluetoothAdapter that represents the device's
+        own Bluetooth adapter (the Bluetooth radio). There's one Bluetooth adapter for the entire system, and your
+        application can interact with it using this object. If getDefaultAdapter() returns null, then the device
+        does not support Bluetooth and your story ends here. For example:
+        */
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Log.d("onCreate", "Device does not support Bluetooth :-(");
+        }else{
+            Log.d("onCreate", "Device support Bluetooth :-D");
+        }
+        /*
+        STEP 2: Enable Bluetooth
+        Next, you need to ensure that Bluetooth is enabled. Call isEnabled() to check whether Bluetooth is currently enable.
+        If this method returns false, then Bluetooth is disabled. To request that Bluetooth be enabled, call
+        startActivityForResult() with the ACTION_REQUEST_ENABLE action Intent. This will issue a request to enable Bluetooth
+        through the system settings (without stopping your application). For example:
+        */
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        }
+
+
     }
 
 
